808754244d6c0a02cb9d04a2dc4b9dede12f2e05
peterjohncherry <peter.cherry@northwestern.edu>
2019-05-15 18:26:32 +0200

    A bit more

--- venv/mat_utils.py
+++ venv/mat_utils.py
@@ -1,23 +1,30 @@
 import numpy as np
 import cmath
 
+
 # generates a real random matrix with nrows and ncols
-def generate_random_matrix(nrows, ncols) :
+def generate_random_matrix(nrows, ncols):
     # type: (int, int) -> matrix
     return np.random.rand(nrows, ncols)
 
-#symmetrizes matrix so it diagonalizes nice like
-def symmetrize_mat( mat ):
-    #type : (numpy.matrix) -> numpy.matrix
-    new_mat = mat/2 + mat.transpose()/2
+
+# symmetrizes matrix so it diagonalizes nice like
+def symmetrize_mat(mat):
+    # type : (numpy.matrix) -> numpy.matrix
+    new_mat = mat / 2 + mat.transpose() / 2
     return new_mat
 
+
 # generates a real random matrix with nrows and ncols
-def generate_random_symmetric_matrix(nrows, ncols) :
+def generate_random_symmetric_matrix(nrows, ncols):
     # type: (int, int) -> matrix
     return symmetrize_mat(np.random.rand(nrows, ncols))
 
 
-#diagonalizes a matrix directly
-def diag (mat ):
+# diagonalizes a matrix directly
+def diag(mat):
     return np.linalg.eig(mat)
+
+def multiply(mat_in, vec_in):
+    return np.mat_mult(mat_in, vec_in)
+
--- venv/test.py
+++ venv/test.py
@@ -4,18 +4,18 @@ import davidson
 import mat_utils as util
 
 
-
 def main():
-    print ( "hello")
-    mat = util.generate_random_symmetric_matrix(3,3)
-    print mat
+    mat = util.generate_random_symmetric_matrix(5,5)
     new_mat = util.symmetrize_mat(mat)
-    print new_mat
     eigvals, eigvecs =  util.diag( new_mat )
-    print ("eigvecs\n")
-    print (eigvecs)
-    print ("eigvals\n")
-    print (eigvals)
+
+    print (mat)
+    print "\n"
+    x = util.generate_random_matrix(5,1)  # type: List[int]
+    print (x)
+    print "\n"
+    y = np.matmul(mat, x)
+    print (y)
 
 main()
 
