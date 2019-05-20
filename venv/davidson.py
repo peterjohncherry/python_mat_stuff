a49f58d50ee088b85a6ee10e93067c32b4b6fb1f
peterjohncherry <peter.cherry@northwestern.edu>
2019-05-20 18:40:25 +0200

    Changed davidson so always using numpy arrays

--- venv/davidson.py
+++ venv/davidson.py
@@ -5,36 +5,29 @@ import mat_utils as util
 #crappy guess vector obtained by slicing row and normalizing
 def get_guess_vec(mat_in, ii):
     nrows = mat_in.shape[0]
-    return util.normalize(mat_in[ii,0:]).reshape(nrows, 1)
+    return util.normalize(mat_in[ii,0:]).reshape(1, nrows)[0]
 
 # Davidson algorithm
 # AA is the matrix whose eigenvectors we seek
 # uu is the list of initial guess vectors
 # ww is the list of vectors defined by AA.uu[j]
 def solve(AA):
-    uu = []
-    ww = []
-    BB = np.array([[0.0]])
-    print ("AA.shape = ", AA.shape[0] )
+    uu = np.array([get_guess_vec(AA,0)])
+    print "uu", uu
+    ww = np.array(util.multiply(uu, AA))
+
+    print "ww", ww
+    BB = np.array([ util.dot(uu, ww) ])                                 # b[j][j] = (u[j]|w[j])
     for jj in range(AA.shape[0]):
-        uu.append(get_guess_vec(AA, jj))
-        ww.append(util.multiply(AA, uu[jj]))            #w[j] = A * u[j]
-        for kk in range(0, jj):
-            BB[kk][jj] = util.dot(uu[kk], ww[jj])                           #b[k][j] = (u[k]|w[j])
-            BB[jj][kk] = util.dot(uu[jj], ww[kk])                           #b[j][k] = (u[j]|w[k])
-        BB[jj][jj] = util.dot(uu[jj], ww[jj])                               #b[j][j] = (u[j]|w[j])
-        BB_tmp = np.array(np.r_['-1', BB, np.zeros((BB.shape[1], 1))])
-        BB = np.array(np.r_['0', BB_tmp, np.zeros((1, BB_tmp.shape[1]))])
-        eigvals, eigvecs = util.diag(BB)
-        print ( "eigvals = ", eigvals)
-        print ("eigvecs = ", eigvecs )
-        SS = util.normalize(eigvecs[:, np.argmax(eigvals)])
-        #YY = util.multiply(uu,SS)        # Ritz vector
-        #AAYY = util.multiply(AA, YY)     #
-        #residual = AAYY- eigvals[np.argmax(eigvals)]*YY
-        #TT = np.diagonal(a).copy()
-        #for ii in range(jj):
-        #    TT[ii] = eigvals[ii]
-        #    TT[ii] = 1/TT[ii]
-
-
+        if jj != 0 :
+            uu = np.r_['0', uu, np.array([get_guess_vec(AA,jj)])]
+            print ("uu = ", uu)
+            print ""
+            ww = util.multiply(AA, uu.transpose())                     #w[j] = A * u[j]
+            print ("ww = ", ww)
+            BB = util.multiply(ww, uu)
+            print "BB = ", BB
+            eigvals, eigvecs = util.diag(BB)
+            print eigvecs, eigvals
+            ss = eigvecs[np.argmax(eigvals),:].transpose()
+            print "ss = ", ss
--- venv/test.py
+++ venv/test.py
@@ -11,33 +11,6 @@ def main():
     nrows = 6
     ncols = 6
     x_mat = util.generate_random_matrix(nrows, ncols)  # type: List[int]
-    print ("x")
-    print (x_mat)
-
-    y_vec = util.generate_random_vector(ncols)
-    print "\n y \n"
-    print (y_vec)
-
-    z_vec = util.multiply(x_mat, y_vec)
-    print "\n z \n"
-    print (z_vec)
-
-    q_scalar = util.multiply(y_vec,z_vec)
-    print "\n q "
-    print (q_scalar)
-
-    print "z_vec_norm np = " + str(util.norm(z_vec))+"\n"
-    print "z_vec_norm mult = " + str(util.multiply(z_vec, z_vec))
-
-    mat_plots = pt.Matrix_Images('x_mat')
-    mat_plots.add_plot(x_mat)
-    x_mat_symm = util.symmetrize_mat(x_mat)
-    mat_plots.add_plot(x_mat_symm)
-    mat_plots.save_imgs()
-
-    sub_mat = util.get_sub_matrix(x_mat, range(2, 3+1), range(4, 5+1))
-    print "sub_mat \n"
-    print sub_mat
 
     dv.solve(x_mat)
 
